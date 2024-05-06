f68f2c8dbf3690a636ac458bf807958594f4f872
Lucas Dunker <dunker.l@northeastern.edu>
2024-05-06 17:03:17 -0400

    Basic grid setup

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,50 @@
-function setup() {
-  background(0);
-  noStroke();
-  fill(102);
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const SAND_COLOR = "#dcb159";
+
+class Grid {
+  initialize(width, height) {
+    this.width = width;
+    this.height = height;
+    this.grid = new Array(width * height).fill(0);
+  }
+
+  // Clear the grid
+  clear() {
+    this.grid = new Array(this.width * this.height).fill(0);
+  }
+
+  // Set a particle at the given x and y coordinate to the given color
+  set(x, y, color) {
+    this.grid[y * this.width + x] = color;
+  }
+
+  // Swap two particles
+  swap(a, b) {
+    const temp = this.grid[a];
+    this.grid[a] = this.grid[b];
+    this.grid[b] = temp;
+  }
+
+  // Check if a particle exists in a space
+  isEmpty(index) {
+    return this.grid[index] === 0;
+  }
 }
 
-function draw() {
-  rect(a++ % width, 10, 2, 80);
+function setup() {
+  frameRate(60);
+  pixelDensity(1);
+  createCanvas(WIDTH, HEIGHT);
+  loadPixels();
+  noCursor();
 }
+
+function draw() {}
+
+function update() {}
+
+function onLeftClick() {}
+
+function onRightClick() {}
